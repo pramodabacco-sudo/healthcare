5cebbb07ec6c3ebc6201b83533b5d1c1d5bfd40a
pramod <pramod.abacco@gmail.com>
2026-07-15 19:04:39 +0530

    update

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./src/index.js";
-
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 const PORT = process.env.PORT || 4000;
 
 
